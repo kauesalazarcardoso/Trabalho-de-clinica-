3652459ac166a86bfa6c8465b284626a4a416a90
Kauê Salazar <kauesalazarcardoso123@gmail.com>
2026-04-24 13:30:29 -0300

    corrige execução dos testes

--- tests/components/test_fila.py
+++ tests/components/test_fila.py
@@ -5,6 +5,18 @@ from src.doctor_repository import DoctorRepository
 from src.patient_repository import PatientRepository
 from src.waitlist_repository import WaitlistRepository
 
+def create_service():
+    doctor_repo = DoctorRepository()
+    patient_repo = PatientRepository()
+    appointment_repo = AppointmentRepository()
+    waitlist_repo = WaitlistRepository()
+
+    return ClinicService(
+        doctor_repo,
+        patient_repo,
+        appointment_repo,
+        waitlist_repo
+    )
 
 def test_entrar_na_fila_com_sucesso():
     service = create_service()
